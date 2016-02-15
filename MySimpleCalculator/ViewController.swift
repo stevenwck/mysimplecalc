8fafed3da41c5f4c31327579929666603bd49fe7
Steven <steven@stevens-imac.local>
2016-02-15 19:19:40 +1100

    Initial Commit

---
+++ MySimpleCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySimpleCalculator
+//
+//  Created by Steven Wong on 15/02/2016.
+//  Copyright © 2016 Steven CK Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
