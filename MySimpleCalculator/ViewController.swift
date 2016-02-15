cece5e89774005157280433737eb4a62ab66b6c4
Steven <steven@stevens-imac.local>
2016-02-15 19:46:20 +1100

    Button press working now

--- MySimpleCalculator/ViewController.swift
+++ MySimpleCalculator/ViewController.swift
@@ -23,14 +23,64 @@ class ViewController: UIViewController {
     }
     
     
-    
     @IBAction func number1Pressed(sender: AnyObject) {
         numberPressed("1")
     }
     
-    func numberPressed(number: String) {
-        numberPanel.text = numberPanel.text! + number
+    @IBAction func number2Pressed(sender: AnyObject) {
+        numberPressed("2")
     }
+    
+    @IBAction func number3Pressed(sender: AnyObject) {
+        numberPressed("3")
+    }
+    
+    @IBAction func number4Pressed(sender: AnyObject) {
+        numberPressed("4")
+    }
+    
+    @IBAction func number5Pressed(sender: AnyObject) {
+        numberPressed("5")
+    }
+    
+    @IBAction func number6Pressed(sender: AnyObject) {
+        numberPressed("6")
+    }
+    
+    @IBAction func number7Pressed(sender: AnyObject) {
+        numberPressed("7")
+    }
+    
+    @IBAction func number8Pressed(sender: AnyObject) {
+        numberPressed("8")
+    }
+    
+    @IBAction func number9Pressed(sender: AnyObject) {
+        numberPressed("9")
+    }
+    
+    @IBAction func number0Pressed(sender: AnyObject) {
+        numberPressed("0")
+    }
+    
+    @IBAction func numberCPressed(sender: AnyObject) {
+        clearNumberPanel()
+    }
+    
+    func numberPressed(number: String) {
+        if (numberPanel.text == "0") {
+            numberPanel.text = number
+        } else {
+            numberPanel.text = numberPanel.text! + number
+        }
+        
+    }
+    
+    func clearNumberPanel() {
+        numberPanel.text = "0"
+    }
+    
+    
 
 
 }
