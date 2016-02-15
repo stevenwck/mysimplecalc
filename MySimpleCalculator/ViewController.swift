64551220a7c7d125c303b0666525e6aa4da93a13
Steven <steven@stevens-imac.local>
2016-02-15 19:36:59 +1100

    First Commit

--- MySimpleCalculator/ViewController.swift
+++ MySimpleCalculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberPanel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func number1Pressed(sender: AnyObject) {
+        numberPressed("1")
+    }
+    
+    func numberPressed(number: String) {
+        numberPanel.text = numberPanel.text! + number
     }
 
 
